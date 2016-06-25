120f063d9b1c7f7197a27b9aa3fb8d33b63c7a64
yang <xyan9484@uni.sydney.edu.au>
2016-06-25 18:58:43 +1000

    teaching demonstrate

--- INFO1103/test.java
+++ INFO1103/test.java
@@ -1,21 +1,6 @@
 public class test{
     public static void main(String[] args){
-      Scanner keyboad = new Scanner( System.in );
-      System.out.print( "Enter a number " );
-      int number = keyboad.nextInt();
-      int sum =  recusionDemo( number );
-      System.out.println(sum);
-    }
-
-    //given a number, compute the sum from 1 up to this number
-    public static int recursionDemo(int number){
-
-      int sum = 0;
-      if ( number == 0 ){
-        return sum;
-      }else {
-        sum += number;
-        recursionDemo( number - 1 );
-      }
+      System.out.print("He Yi Jun");
+      System.out.println("He Jun Yi");
     }
 }
