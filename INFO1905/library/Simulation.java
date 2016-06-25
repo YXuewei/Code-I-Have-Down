99bb214c661a079285e5dd757ed995f7697f0667
yang <xyan9484@uni.sydney.edu.au>
2016-06-25 18:59:03 +1000

    finished add command, working on edit command

--- INFO1905/library/Simulation.java
+++ INFO1905/library/Simulation.java
@@ -14,8 +14,33 @@ public class Simulation{
     }
 
     public static void help(){
-        System.out.println("Set < title >");
+        System.out.println("Add < title >");
         System.out.println("Edit < title >");
+        System.out.println("Borrow < title >");
+        System.out.println("Return < title >");
+    }
+
+    public static void add(String input){
+        
+        String[] information = input.split(" ");
+        Arraylist<String> toAdd = new ArrayList<String>();
+        for ( int i = 0; i < 6; i++){
+            if ( i > information.length ){
+                toAdd.add( "Unkown" );
+            }else{
+                toAdd.add( information[i] );
+            }
+        }
+        String informaton = toAdd.toArray();
+        
+        Book newBook = new Book( information[0], information[1], information[2], information[3], information[4]
+                                 information[5] );
+        library.put( information[0], newBook);
+
+        System.out.println("Book added!");
+    }
+
+    public static void Edit( String input ){
         
     }
 }
