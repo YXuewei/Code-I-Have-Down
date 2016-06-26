94c52554fb71fd997760521b14cdff1f4fab4d54
yang <xyan9484@uni.sydney.edu.au>
2016-06-27 00:55:22 +1000

    standard input and output

--- C_Language/HelloWorld.c
+++ C_Language/HelloWorld.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
 main(){
-    float fahr, celsius;
-    int lower , upper, step;
-
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-
-    printf("Table of temperatur\n");
-    printf("Fahr\tCelsius\n");
-    while ( fahr <= upper ){
-        celsius = ( 5.0 / 9.0 ) * ( fahr - 32.0 );
-        printf( "%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }   
+    int c;
+    while ( c != EOF){
+        c = getchar();
+        putchar( c );
+    }
 }
