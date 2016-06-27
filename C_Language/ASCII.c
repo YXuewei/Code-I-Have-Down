6a9ecd785760e43dcd49c4757bae3196ea1ad940
yang <xyan9484@uni.sydney.edu.au>
2016-06-28 07:08:53 +1000

    type in a character print out it's ASCII code

---
+++ C_Language/ASCII.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+main(){
+
+    char input;
+    printf( "Now type in a character I will show you it's ASC-II code " );
+    scanf("%c", &input);
+    printf( "ASC-II Code for %c is %d",input, input);
+}
