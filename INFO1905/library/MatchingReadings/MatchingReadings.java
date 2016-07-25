5b4972c1d2e0ac93f56e4b4db4fe0acee3f05075
yang <xyan9484@uni.sydney.edu.au>
2016-07-26 01:12:16 +1000

    INFO1905 practice Matching Readings

---
+++ INFO1905/library/MatchingReadings/MatchingReadings.java
@@ -0,0 +1,56 @@
+public class MatchingReadings{
+
+public   static int countMatchingReadings(List<Report> reportList, int startTime, int endTime) {
+    //TODO: return the number of readings which were logged between startTime and endTime (inclusive)
+    int diff = endTime - startTime;
+    int count = 0;
+    for ( Report rep : reportList){
+        if ( rep.getTime() <= diff ){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+public static int maxMatchingReadings(List<Report> reportList, int startTime, int endTime) {
+    //TODO: return the maximum value of any reading which was logged between startTime and endTime (inclusive)  
+    int diff = endTime - startTime;
+    int max = Integer.MIN_VALUE;
+    for ( Report rep : reportList ){
+        if ( rep.getTime() <= diff ){
+            if ( rep.getValue() > max ){
+                max = rep.getValue();
+            }
+        } 
+    }
+
+    return max;
+
+}
+public static int modeMatchingReadings(List<Report> reportList, int startTime, int endTime) {
+   //TODO: return the mode (most common value) of any reading which was logged between startTime and endTime (inclusive)  
+   int diff = endTime - startTime;
+   HashMap< Integer, Integer > mode = new HashMap<Integer, Integer>();
+   for ( Report rep : reportList ){
+       if ( rep.getTime() <= diff ){
+           if ( mode.containsKey() ){
+               int value = mode.get( rep.getValue() ) + 1;
+               mode.put( rep.getValue(), value );
+           }else{
+               mode.put( rep.getValue(), 1);
+           }
+       }
+   }
+
+   int max = Integer.MIN_VALUE;
+   int[] requency = mode.values().toArray();
+   for ( int i : requency ){
+       if ( i > max ){
+           max = i;
+       }
+   }
+
+   return max;
+}
+}
---
+++ INFO1905/library/MatchingReadings/Report.java
@@ -0,0 +1,18 @@
+public class Reprt{
+    private int time;
+    private int value;
+
+    public Report(int time, int value) {
+        this.time = time;
+        this.value = value;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
