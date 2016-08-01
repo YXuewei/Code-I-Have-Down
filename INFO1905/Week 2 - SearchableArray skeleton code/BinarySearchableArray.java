76271a9fee509adbf6663498378bfb89ec1b4fb1
yang <xyan9484@uni.sydney.edu.au>
2016-08-01 16:59:19 +1000

    Tutorial for Linear search and Binary Search

---
+++ INFO1905/Week 2 - SearchableArray skeleton code/BinarySearchableArray.java
@@ -0,0 +1,18 @@
+
+public class BinarySearchableArray<T extends Comparable<T>> extends SearchableArray<T> {
+
+	// constructor
+	public BinarySearchableArray(T[] data) {
+		super(data); //call the constructor of the super-type as SearchableArray<T>(data)
+	}
+
+	@Override
+	public T search(T target) {
+		
+		// your code goes here.
+		// to help implement this recursively, you may want to add another method
+		
+		return null; //if the search failed, return null
+	}
+
+}
---
+++ INFO1905/Week 2 - SearchableArray skeleton code/LinearSearchableArray.java
@@ -0,0 +1,24 @@
+
+/*
+ * This class signature means:
+ * 1) LinearSearchableArray uses a generic type T which must be comparable
+ * and
+ * 2) LinearSearchableArray extends SearchableArray (with the same comparable type T)
+ */
+public class LinearSearchableArray<T extends Comparable<T>> extends SearchableArray<T> {
+
+	// constructor
+	public LinearSearchableArray(T[] data) {
+		super(data); //call the constructor of the super-type as SearchableArray<T>(data)
+	}
+	
+	// implement the abstract method from SearchableArray
+	@Override
+	public T search(T target) {
+		
+		// your code goes here
+		
+		return null; //if the search failed, return null
+	}
+
+}
