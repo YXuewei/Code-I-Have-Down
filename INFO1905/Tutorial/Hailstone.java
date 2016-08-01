0f3026739b2197a395131ef8415216219bbfd17b
yang <xyan9484@uni.sydney.edu.au>
2016-08-01 23:17:11 +1000

    return a list of sequenced Hailstone number

---
+++ INFO1905/Tutorial/Hailstone.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Hailstone{
+    
+    public static List<Integer> hailstone( int n ){
+       ArrayList<Integer> list = new ArrayList<Integer>();
+       boolean stop = false;
+       if ( n == 1 ){
+           list.add( n );
+           return list;
+       }
+       
+       while ( stop == false){
+            list.add( n );
+            if ( n % 2 == 0 ){
+                n = n / 2;
+                if ( n == 1){
+                    stop = true;
+                    list.add( n );
+                }
+                continue;
+            }else{
+                n = n * 3 + 1;
+                continue;
+            }
+        }
+
+        return list;
+    }
+}
