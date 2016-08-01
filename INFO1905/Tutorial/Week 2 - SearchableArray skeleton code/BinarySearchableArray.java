6e4b16700c6edd30b2370fb9ea874e0f806aa0c7
yang <xyan9484@uni.sydney.edu.au>
2016-08-01 22:52:30 +1000

    Binary search implementation, but faild eficciency

--- INFO1905/Tutorial/Week 2 - SearchableArray skeleton code/BinarySearchableArray.java
+++ INFO1905/Tutorial/Week 2 - SearchableArray skeleton code/BinarySearchableArray.java
@@ -8,11 +8,34 @@ public class BinarySearchableArray<T extends Comparable<T>> extends SearchableAr
 
 	@Override
 	public T search(T target) {
-		
-		// your code goes here.
-		// to help implement this recursively, you may want to add another method
+
+		int len = this.data.length;
+		int result = this.BinarySearch( target, 0, len - 1);
+
+		if (result == 0){
+			return target;
+		}
 		
 		return null; //if the search failed, return null
+	}
+
+
+	// 0 means find the target
+	// 1 means did not find the target
+	public int BinarySearch( T target, int lowerBound, int upperBound ){
+		if ( lowerBound <= upperBound ){
+			int mid = ( lowerBound + upperBound ) / 2;
+			int result = this.data[ mid ].compareTo( target );
+			if ( result == 0 ){
+				return 0;
+			} else if ( result < 0 ){
+				return this.BinarySearch( target, mid + 1, upperBound );
+			}else{
+				return this.BinarySearch( target, lowerBound, upperBound - 1 );
+			}
+		}else{
+			return 1;
+		}
 	}
 
 }
