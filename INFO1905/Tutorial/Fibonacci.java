301b12fecad2415d610224e8985ac4c6547afe3a
yang <xyan9484@uni.sydney.edu.au>
2016-08-16 12:41:28 +1000

    Fibonacci series with stack operation(MUCH FASTER)

--- INFO1905/Tutorial/Fibonacci.java
+++ INFO1905/Tutorial/Fibonacci.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 public class Fibonacci{
     
-    public static void main( String[] args ){
+    /*public static void main( String[] args ){
         Scanner keyboard = new Scanner( System.in);
         int input;
         System.out.printf( "Input a number\t");
         input = keyboard.nextInt();
         System.out.print(" Result is " + fibonacci( input ) +"\n");
-    }
+    }*/
+
+    public static Stack<Integer> stacks = new Stack<>();
     
     public static int fibonacci( int n ){
         if ( n == 0 ){
@@ -18,5 +20,26 @@ public class Fibonacci{
         }else {
             return  fibonacci( n - 1 ) + fibonacci( n - 2 );
         }
+    }
+
+    public static Stack<Integer> getNumbers( int n ){
+       if ( n < 0 ){
+           return stacks;
+       }
+
+       for ( int i = 0; i <= n; i++ ){
+           if ( i < 2 ){
+               stacks.push( i );
+           }else{
+               int a = stacks.pop().intValue();
+               int b = stacks.pop().intValue();
+               int c = a + b;
+               stacks.push( b );
+               stacks.push( a );
+               stacks.push( c );
+           }
+       }
+
+       return stacks;
     }
 }
