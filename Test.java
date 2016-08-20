ba04717d643e17e91c8a78bf918449cedfce7652
unknown <>
2016-08-21 05:53:58 +1000

    function test

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
+import java.util.*;
+import java.lang.*;
+
 public class Test{
     public static void main( String[] args){
-        System.out.println( 1 % 2 );
+       String str = "  a   b c    d";
+       str = str.replaceAll( "[ ]{2,}","");
+       System.out.println( str );
     }
 }
