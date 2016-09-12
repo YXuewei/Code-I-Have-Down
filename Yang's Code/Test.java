52e6bac0d77cb108a7c466cd209aaf53563d93c3
unknown <>
2016-09-13 00:37:27 +1000

    simple Test for Hashtable

--- Yang's Code/Test.java
+++ Yang's Code/Test.java
@@ -3,11 +3,10 @@ import java.lang.*;
 
 public class Test{
     public static void main( String[] args){
-       int[] a = { 0, 1, 2, 3, 4, 5};
-       int low = 0;
-       while( low < a.length ){
-           int p = a[ low ++ ];
-           System.out.println( p );
-       }
+        Hashtable< Date, String> a = new Hashtable< Date, String>();
+        Date today = new Date( 2016, 12, 20 );
+        String location = "Sydney";
+        a.put( today, location );
+        System.out.println( a.size() );
     }
 }
