8e8618448fc9ade5ac08d06c458a5adbed7535a5
yang <xyan9484@uni.sydney.edu.au>
2016-09-12 16:16:43 +1000

    reorgnized file

---
+++ Yang's Code/INFO1905/Tutorial/BstSimpleSortedMap.java
@@ -0,0 +1,224 @@
+
+
+
+public class BstSimpleSortedMap implements SimpleSortedMap {
+	
+	private class MySimpleEntry implements SimpleEntry {
+		
+		private final Integer key;
+		private final String value;
+		private MySimpleEntry left;
+		private MySimpleEntry right;
+		private MySimpleEntry parent;
+		
+		MySimpleEntry(Integer key, String value) {
+			this.key = key;
+			this.value = value;
+			this.left = null;
+			this.right = null;
+			this.parent = null;
+		}
+
+		@Override
+		public Integer getKey() { return key; }
+
+		@Override
+		public String getValue() { return value; }
+		
+		protected MySimpleEntry getLeft() { return left; }
+		protected MySimpleEntry getRight() { return right; }
+		protected MySimpleEntry getParent() { return parent; }
+
+		protected void setLeft(MySimpleEntry entry) { left = entry; }
+		protected void setRight(MySimpleEntry entry) { right = entry; }
+		protected void setParent(MySimpleEntry entry) { parent = entry; }
+	}
+
+	private int size;
+	private MySimpleEntry root;
+	
+	public BstSimpleSortedMap() {
+		size = 0;
+		root = null;
+	}
+	
+	// attaches the subtree rooted at 'child', to the parent
+	private void attachLeft(MySimpleEntry parent, MySimpleEntry child) {
+		if(child != null) { child.setParent(parent); }
+		if(parent != null) { parent.setLeft(child); }
+	}
+
+	// attaches the subtree rooted at 'child', to the parent
+	private void attachRight(MySimpleEntry parent, MySimpleEntry child) {
+		if(child != null) { child.setParent(parent); }
+		if(parent != null) { parent.setRight(child); }
+	}
+
+	//////////////////////////
+	// Map ADT methods:
+	//////////////////////////
+	
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public String get(Integer k) {
+		return get(k, root);
+	}
+	
+	private String get(Integer k, MySimpleEntry subtreeRoot) {
+		// base case: empty subtree
+		if(subtreeRoot == null) {
+			// k isn't in this subtree
+			return null;
+		}
+		
+		// base case: k matches the key in the current entry
+		if(k.compareTo(subtreeRoot.getKey()) == 0) {
+			// return the value of the current entry
+			return subtreeRoot.getValue();
+		}
+		// recursive case: k < the current entry
+		else if(k.compareTo(subtreeRoot.getKey()) < 0) {
+			// return the result of recursing to the left
+			return get(k,subtreeRoot.getLeft());
+		}
+		// recursive case: k > the current entry
+		else {
+			// return the result of recursing to the right
+			return get(k,subtreeRoot.getRight());
+		}
+	}
+
+	@Override
+	public String put(Integer k, String v) {
+		// We will implement this recursively.
+		
+		// If the key already exists, we will need to return the old value
+		String oldValue = get(k);
+		
+		// Replace the subtree rooted at 'root' with
+		// the resulting subtree after doing the put
+		root = put(k, v, root);
+		
+		return oldValue;
+	}
+	
+	// Recursive helper method for put
+	// Returns the subtree rooted at entry after performing the put
+	private MySimpleEntry put(Integer k, String v, MySimpleEntry subtreeRoot) {
+		// base case: the key wasn't in the subtree
+		if(subtreeRoot == null) {
+			// we have reached a null subtree, where k should be
+			//create a new entry
+			MySimpleEntry entry = new MySimpleEntry(k,v);
+			//increment the size variable
+			size = size + 1;
+			//return the new entry			
+			return entry;			
+		}
+
+		// base case: k matches the one in the current entry
+		if(k.compareTo(subtreeRoot.getKey()) == 0) {
+			// create a new entry
+			MySimpleEntry entry = new MySimpleEntry(k,v);
+			// attach the left child of the current entry to it	
+			attachLeft(entry,subtreeRoot.getLeft());		
+			// attach the right child of the current entry to it	
+			attachRight(entry,subtreeRoot.getRight());		
+			// return the new subtree			
+			return entry;
+		}
+		// recursive case: k < the current entry
+		else if(k.compareTo(subtreeRoot.getKey()) < 0) {
+			// get the subtree resulting from recursing left
+			MySimpleEntry subtree = put(k,v,subtreeRoot.getLeft());
+			// attach that subtree to the current entry
+			attachLeft(subtreeRoot,subtree);
+			// return the modified entry
+			return subtreeRoot;
+		}
+		// recursive case: k > the current entry
+		else {
+			// get the subtree resulting from recursing right
+			MySimpleEntry subtree = put(k,v,subtreeRoot.getRight());
+			// attach that subtree to the current entry
+			attachRight(subtreeRoot,subtree);
+			// return the modified entry
+			return subtreeRoot;
+		}
+	}	
+
+	@Override
+	public String remove(Integer k) {
+		// TODO implement this in exercise 2
+		// Implement it recursively.
+		return null;
+	}
+
+	@Override
+	public Iterable<Integer> keySet() {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterable<String> values() {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterable<SimpleEntry> entrySet() {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	//////////////////////////
+	// SortedMap ADT methods:
+	//////////////////////////
+	
+	@Override
+	public SimpleEntry firstEntry() {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public SimpleEntry lastEntry() {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public SimpleEntry ceilingEntry(Integer key) {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public SimpleEntry floorEntry(Integer key) {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public SimpleEntry lowerEntry(Integer key) {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public SimpleEntry higherEntry(Integer key) {
+		throw new java.lang.UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterable<Integer> subMap(Integer fromKey, Integer toKey) {
+		throw new java.lang.UnsupportedOperationException();
+	}
+	
+	
+	
+	
+
+}
