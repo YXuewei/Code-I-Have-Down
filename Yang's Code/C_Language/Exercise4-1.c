ac2e8f50026dde707f60a1b37e3a7c00ac82d0d2
unknown <>
2017-01-26 04:06:25 +1100

    Clang-format my code

--- Yang's Code/C_Language/Exercise4-1.c
+++ Yang's Code/C_Language/Exercise4-1.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #define MAX 30
 
-int strrindex( char s[], char t);
+int strrindex(char s[], char t);
 
-int main(){
+int main()
+{
     char line[MAX];
     char search;
-    int p; 
-    
+    int p;
+
     printf("Enter a line\n");
-    scanf("%s",line);
+    scanf("%s", line);
     printf("Enter a char that would like to search\n");
     getchar();
-    scanf("%c",&search);
+    scanf("%c", &search);
 
-    p = strrindex( line, search);
-    printf("%d", p );
-
+    p = strrindex(line, search);
+    printf("%d", p);
 }
 
-int strrindex( char s[], char t){
+int strrindex(char s[], char t)
+{
     int position = -1;
-    for ( int i = 0; s[i] != '\0'; ++i){
-       if ( s[i] == t){
-           position = i;
-       }
-   } 
-   return position;
+    for (int i = 0; s[i] != '\0'; ++i)
+    {
+        if (s[i] == t)
+        {
+            position = i;
+        }
+    }
+    return position;
 }
