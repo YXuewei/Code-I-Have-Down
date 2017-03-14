1c323aec31d4b671b29e2cac66b9e9fb94964845
unknown <>
2017-03-14 20:04:16 +1100

    finished task1

--- Yang's Code/COMP2129/bouncy_string.c
+++ Yang's Code/COMP2129/bouncy_string.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main ( int argc, char *argv[] )
 {
     if ( argc != 4 )
+    {
         printf( "Invalid arguments number\n");
-    else if ( atoi(argv[2]) < 0 )
-        printf( "Invalid start position\n");
-    else if ( atoi(argv[3]) < 0 )
+        return 1;
+    }
+    else if( atoi(argv[2]) < 0 )
+    {
+         printf( "Invalid start position\n");
+    }
+    else if( atoi(argv[3]) < 0 )
+    {    
         printf( "Invalid iteration count");
+    }
     else
     {   
-        printf("%s,%s", argv[2], argv[3] );
-        printf("Yes");
+        int stpo = atoi( argv[ 2 ] );
+        int iteco = atoi( argv[3] );
+        int len = strlen( argv[1]);
+        char str[ len + 1] = &argv[1];
+        int isRev = 0;
+        
+        for(int i = 0; i < iteco; i++ )
+        {
+            putchar( str[stpo]);
+            if( stpo == len - 1)
+                isRev = 1;
+            else if( stpo == 0 )
+                isRev = 0;
+            if( isRev == 1)
+                stpo --;
+            else
+                stpo++;
+        }
     }
 }
