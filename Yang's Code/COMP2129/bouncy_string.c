b11e4a35cdcddccc4f08db269910a4a11bd163b7
unknown <>
2017-03-14 19:04:44 +1100

    conversion bug fixed

--- Yang's Code/COMP2129/bouncy_string.c
+++ Yang's Code/COMP2129/bouncy_string.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ( int argc, char *argv[] )
 {
     if ( argc != 4 )
         printf( "Invalid arguments number\n");
-    else if ( ( argv[2] - '0') < 0 )
+    else if ( atoi(argv[2]) < 0 )
         printf( "Invalid start position\n");
-    else if ( (argv[3] - '0') < 0 )
+    else if ( atoi(argv[3]) < 0 )
         printf( "Invalid iteration count");
     else
-    {
+    {   
+        printf("%s,%s", argv[2], argv[3] );
         printf("Yes");
     }
 }
