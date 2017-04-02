79b868f21823e738543b9f82c2e247ca5917acac
Xuewei Yang <xyan@XueweimatoMBP.telstra.com.au>
2017-04-02 21:20:31 +1000

    Scanf code for task index

---
+++ COMP2129/index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+//You may add/modify/remove structs
+
+typedef struct pixel_t pixel_t;
+
+struct pixel_t {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+	uint8_t empty;
+};
+
+int main(int argc, char** argv) {
+	
+	// TODO
+	return 0;
+}
