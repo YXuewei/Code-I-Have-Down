572b43ea383065eeb15333a77aede39ddbc786e7
unknown <>
2017-04-06 00:58:41 +1000

    first attemp asm 2

--- COMP2129/Assignment1/atoms.c
+++ COMP2129/Assignment1/atoms.c
@@ -1,7 +1,216 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "atoms.h"
 
-int main(int argc, char** argv) {
 
+
+
+void print_help();
+void start( char *input );
+int command_check( const char *input );
+void take_input( char *input );
+
+int main(int argc, char** argv)
+{
+enum commands{ HELP, QUIT, DISPLAY, START, PLACE, UNDO, STAT,
+                        SAVE, LOAD, PLAYFROM} command;
+
+  char input[255];
+  while ( 1)
+  {
+    take_input( input );
+    command = command_check( input );
+    switch( command )
+    {
+      case HELP:
+        print_help();
+        break;
+      case QUIT:
+        return 0;
+      case DISPLAY:
+        return 0;
+      case START:
+        start( input );
+        break;
+      case PLACE:
+        return 0;
+      case UNDO:
+        return 0;
+      case STAT:
+        return 0;
+      case SAVE:
+        return 0;
+      case LOAD:
+        return 0;
+      case PLAYFROM:
+        return 0;
+      default:
+        printf("Invalid command\n");
+        break;
+    }
+  }
+
+  
   return 0;
+}
+
+void print_help()
+{
+  printf("HELP    displays this help message\n");
+  printf("Quit    quits the current game\n");
+  printf("\n");
+  printf("DISPLAY    draws the game board in terminal\n");
+  printf("START <width> <height> <number of players>    starts the game\n");
+  printf("PLACE <x> <y>    places anatom in a grid space\n");
+  printf("UNDO    undoes the last move made\n");
+  printf("STAT    displays game statistics\n");
+  printf("\n");
+  printf("SAVE <filename>    saves the state of the game\n");
+  printf("LOAD <filename>    loads a save file\n");
+  printf("PLAYFROM <turn>    plays from n steps into the game\n");
+}
+
+void start( char *input )
+{
+  int array[3];
+  int w,h,np,count;
+  count = 0;
+  char *token;
+  char *ptr;
+  token = strtok( input, " ");
+  //passing 3 times to assign variable w,h,np,count 
+  while( token != NULL )
+  {
+    if ( count > 3 )
+    {
+      printf("Too Many Arguments\n");
+      return;
+    }
+    
+    token = strtok( NULL, " ");
+    ptr = token;
+    array[count] = strtol(token, &ptr, 10);
+    if ( ptr == token )
+    {
+      printf("Invalid command arguments\n");
+      return;
+    }
+    count++;
+  }
+
+  if ( count != 3 )
+  {
+    printf("Too Many Arguments\n");
+    return;
+  }
+
+  w = array[0];
+  h = array[1];
+  np = array[2];
+
+  if ( w <0 || h <0 || np < 0)
+  {
+    printf("Invalid command argumetns\n");
+    return;
+  }
+
+  if ( w * h < np )
+  {
+    printf("Cannot Start Game\n");
+    return;
+  }
+  
+  printf("Game Ready\n");
+  
+
+}
+
+int command_check( const char *input )
+{
+  char *token;
+  char *cp = (char*)malloc( 255*sizeof(char));
+  strcpy(cp, input );
+  token = strtok( cp, " ");
+    if ( strcmp( token, "HELP") == 0)
+    {
+      free(cp);
+      return 0;
+    }
+
+    if ( strcmp( token, "Quit") == 0 )
+    {
+      free(cp);
+      return 1;
+    }
+
+    if ( strcmp( token, "DISPLAY") )
+    {
+      free(cp);
+      return 2;
+    }
+
+    if ( strcmp( token, "START") )
+    {
+      free(cp);
+      return 3;
+    }
+
+    if ( strcmp( token, "PLACE") )
+    {
+      free(cp);
+      return 4;
+    }
+
+    if( strcmp( token, "UNDO") )
+    {
+      free(cp);
+      return 5;
+    }
+
+    if ( strcmp( token, "STAT") )
+    {
+      free(cp);
+      return 6;
+    }
+
+    if ( strcmp( token, "SAVE") )
+    {
+      free(cp);
+      return 7;
+    }
+
+    if ( strcmp( token, "LOAD") )
+    {
+      free(cp);
+      return 8;
+    }
+
+    if ( strcmp( token, "PLAYFROM") )
+    {
+      free(cp);
+      return 9;
+    }
+
+    free(cp);
+    return 10;
+}
+
+void take_input( char *input )
+{
+  int c;
+  int count = 0; 
+  while ( ( c = getchar() ) != EOF )
+  {
+    if ( c == '\n')
+    {
+      input[count] = '\0';
+      break;
+    }
+    else
+    {
+      input[count] = c;
+      count++;
+    }
+  }
 }
