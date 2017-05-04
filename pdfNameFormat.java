f6dcdbd4643ba3d0d36c445f16eaf5986c9e0705
unknown <>
2017-05-04 18:20:15 +1000

    take input from command line argument

--- pdfNameFormat.java
+++ pdfNameFormat.java
@@ -3,7 +3,6 @@ import java.lang.*;
 
 public class pdfNameFormat{
     public static void main( String[] args){
-        String str = "Introduction_to_the_Theory_of_Computation_by_Michael_Sipser_Third_Edition_Course_Technology";
-        System.out.println( str.replaceAll("_", " ") );
+        System.out.println( args[0].replaceAll("_", " ") );
     }
 }
